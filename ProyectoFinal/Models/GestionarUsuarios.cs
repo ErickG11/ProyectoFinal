a1926a96e63771d0f4b9f4d21fefca17abcd43d0
ErickG11 <grandaerick8@gmail.com>
2024-10-31 09:04:49 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoFinal/Models/GestionarUsuarios.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoFinal.Models
+{
+    public class GestionarUsuarios
+    {
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Contraseña { get; set; }
+    }
+}
