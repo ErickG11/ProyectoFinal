aadd8c818e3f95509365b2ac49cd37b9d98332db
ErickG11 <grandaerick8@gmail.com>
2024-11-01 10:59:08 -0500

    servicio de usuario

---
+++ ProyectoFinal/Servicios/Contrato/IUsuarioService.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using ProyectoFinal.Models;
+
+namespace ProyectoFinal.Servicios.Contrato
+{
+    public interface IUsuarioService
+    {
+        Task<Usuario> GetUsuario(string correo, string clave); //Devolver Usuario
+        Task<Usuario> SaveUsuario(Usuario modelo); //Guarda Usuario
+    }
+}
---
+++ ProyectoFinal/Servicios/Implementacion/UsuarioService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Query.Internal;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinal.Models;
+using ProyectoFinal.Servicios.Contrato;
+
+namespace ProyectoFinal.Servicios.Implementacion
+{
+    public class UsuarioService : IUsuarioService
+    {
+        private readonly DbloginContext _dbContext;
+        public UsuarioService(DbloginContext dbContext) //Constructor
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Usuario> GetUsuario(string correo, string contraseña)
+        {
+            Usuario usuario_encontrado = await _dbContext.Usuarios.Where(u => u.Correo == correo && u.Contraseña == contraseña)
+                .FirstOrDefaultAsync();
+            return usuario_encontrado;
+        }
+
+        public async Task<Usuario> SaveUsuario(Usuario modelo)
+        {
+            _dbContext.Usuarios.Add(modelo);
+            await _dbContext.SaveChangesAsync();
+            return modelo;
+        }
+    }
+}
